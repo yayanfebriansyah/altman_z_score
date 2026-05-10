ca7db0e3fb6e4abf82eeefff221e2b6acbf1599e
yayanfebriansyah <67580768+yayanfebriansyah@users.noreply.github.com>
2026-05-10 22:51:47 +0700

    Update z_and_f_score.py

--- z_and_f_score.py
+++ z_and_f_score.py
@@ -113,7 +113,7 @@ def calculate_piotroski(ticker, info):
 col1, col2 = st.columns([1, 5])
 
 with col1:
-    st.image("logo-bursa-efek-indonesia-final.webp", width=120)
+    st.image("logo-bursa-efek-indonesia-final.webp", width=150)
 
 with col2:
     st.title("Financial Health & Fundamental Dashboard")
